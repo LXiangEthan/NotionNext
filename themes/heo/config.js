5658fbd73a6c35ee6453377455601ae9fd1a1b90
Ethan <166964157+LXiangEthan@users.noreply.github.com>
2024-09-27 12:02:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'door.ethanliang.top',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'Ethan's Share About',
+  HEO_HERO_TITLE_5: 'Ethan Share About',
   HEO_HERO_TITLE_LINK: 'https://door.ethanliang.top',
 
   // 英雄区显示三个置顶分类
